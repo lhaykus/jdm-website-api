c398b823673d0e7d533db3aaebb3cc8e53557ab9
Loryn <lorynhaykus@yahoo.com>
2022-06-14 19:29:56 -0600

    adding and using jwt to add more protection

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const User = require('../models/User');
 //Using CryptoJS to hash passwords
 const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
 
 //Sign up
@@ -16,6 +17,7 @@ router.post('/signup', async (req, res) => {
     });
 
     try {
+        //saving new user data
         const savedUser = await newUser.save();
         res.status(200).json(savedUser)
 
@@ -40,13 +42,26 @@ router.post('/login', async (req, res) => {
             process.env.SECRET_PASS);
 
         //converting hashed password to a string
-        const password = hashedPassword.toString(CryptoJS.enc.Utf8);
+        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
 
         //if password does not equal req.body.password send error
-        password !== req.body.password && res.status(401).json('wrong password and username');
-     
+        originalPassword !== req.body.password && res.status(401)
+        .json('wrong password and username');
+
+        //giving each user a unique token to give permission for user to delete/add/etc
+        const accessToken = jwt.sign({
+            id: user._id,
+            isAdmin: user.isAdmin,
+        }, process.env.JWT_KEY,
+        //toekn expires in 3days and user will have to re login
+        {expiresIn:'3d'}
+        );
+    
+        //to hide password and show other information except password
+        const { password, ...others } = user._doc;
+        
         //if username and password match send back user
-        res.status(200).json(user);
+        res.status(200).json({...others, accessToken});
 
     } catch (error) {
         res.status(500).json(error);
